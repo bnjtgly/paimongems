c29fabb891215a8447dfaf7b247bfacf707b5e06
bnjtgly <benjibtigley@gmail.com>
2021-06-19 11:16:21 +0800

    Fixed some issues with loader

--- app/javascript/planner/components/App.jsx
+++ app/javascript/planner/components/App.jsx
@@ -17,9 +17,9 @@ function App() {
 
 	useEffect(
 		() => {
-		  let timer1 = setTimeout(() => setShow(true), 3000);
+		  let timer = setTimeout(() => setShow(true), 1000);
 		  return () => {
-			clearTimeout(timer1);
+			clearTimeout(timer);
 		  };
 		},
 		[]
